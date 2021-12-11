5cb179c040863e74baea940c250db3eba46104f5
that-webdev-dude <that.webdev.dude@gmail.com>
2021-12-11 20:02:23 +0000

    post css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+
+const isProductionMode = process.env.NODE.ENV === 'production';
 
 module.exports = {
   entry: './src/index.js',
@@ -6,16 +9,44 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: isProductionMode ? '[name].[contenthash].css' : '[name].css',
+    }),
+  ],
   module: {
     rules: [
+      /**
+       * styles
+       */
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          isProductionMode
+            ? MiniCSSExtractPlugin.loader
+            : { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+            },
+          },
+          { loader: 'postcss-loader' },
+        ],
       },
+
+      /**
+       * images
+       */
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+
+      /**
+       * fonts
+       */
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
