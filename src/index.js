772433c2ab919ce50e74dbf4565344469800372b
that-webdev-dude <that.webdev.dude@gmail.com>
2021-12-11 19:17:10 +0000

    font loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './style.css';
+import Pic from './web-dev-dude.jpg';
 
 function component() {
+  // main div
   const element = document.createElement('div');
   element.textContent = 'Hello Webpack';
   element.classList.add('hello');
+
+  // add image
+  const myPic = new Image();
+  myPic.src = Pic;
+  element.appendChild(myPic);
+
   return element;
 }
 
