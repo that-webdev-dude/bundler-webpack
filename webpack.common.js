bfb2b773dd7b32ce3b77e59a46542940caa4842e
that-webdev-dude <that.webdev.dude@gmail.com>
2021-12-14 07:27:33 +0000

    multipage support

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const devMode = process.env.NODE_ENV !== 'production';
-const devMode = false;
+const devMode = process.env.NODE_ENV !== 'production';
 const { pages } = require('./project.config');
 
 module.exports = {
